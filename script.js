ab18ced44554cf3e55637fae34c543a4a50da499
JK-8989 <jiyoungkim365@gmail.com>
2022-06-01 21:26:51 -0700

    Edit tilt effects

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
-const overlay = document.getElementById('lightbox-overlay');
-const images = document.querySelectorAll('li > img');
-const fullImage = document.getElementById('full-image');
-console.log(fullImage)
+const overlay = document.getElementById("lightbox-overlay");
+const images = document.querySelectorAll(".content > img");
+const fullImage = document.getElementById("full-image");
+console.log(fullImage);
 
 images.forEach((images) => {
-  images.addEventListener('click', ()=>{
-    overlay.classList.add('active');
+  images.addEventListener("click", () => {
+    // overlay.classList.remove('card');
+    overlay.classList.add("active");
 
-      let target = images.target,
-          targetId = images.getAttribute('src');
-          console.log(targetId)
-          fullImage.src = targetId;
+    let target = images.target,
+      targetId = images.getAttribute("src");
+    console.log(targetId);
+    fullImage.src = targetId;
 
-   
+    overlay.addEventListener("click", () => {
+      overlay.classList.remove("active");
+      // overlay.classList.add('card');
+    });
+  });
+});
 
-    overlay.addEventListener('click', ()=>{
-      overlay.classList.remove('active');
-    })
-  })
-})
-
-console.log(images)
+console.log(images);
 console.log(overlay);
 console.log("connected");
